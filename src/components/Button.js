2d64dc1ef3f7d422eb23ab293838030eb0e620fc
Isaque F <isaquefrankli@gmail.com>
2021-05-05 22:19:02 -0300

    first one

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Button({ children }) {
+
+    const handleClick = null;
+
+    return (
+        <button onClick={handleClick}>{children}</button>
+    )
+}
+
+export default Button
